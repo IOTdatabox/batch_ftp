8383f6a8faea111b7f54a3e8c873896711fc9126
ayapro0330 <ayapro0330@gmail.com>
2025-03-14 12:17:38 +0800

    fix: ip reconfig

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { generateCSV } from './services/signup.service';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT as string) || 5000;
 
 // Middleware
 app.use(express.json());
@@ -27,6 +27,6 @@ app.use(express.static(publicPath));
 app.use('/api', router);
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
